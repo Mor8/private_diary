e97a61eaeb8bf0afad76c1c91e073025b77943a2
Mor8 <fukuya.mori@zeiss.com>
2020-06-14 21:14:38 +0900

    [No.2782]問い合わせ用のルーティングを追加するため

--- diary/urls.py
+++ diary/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'diary' # diaryアプリケーションのルーティング名
 urlpatterns = [
     path('', views.IndexView.as_view(), name = "index"),
+    path('inquiry', views.IndexView.as_view(), name="inquiry"), # 問い合わせ用のルーティングを追加  
 ]
