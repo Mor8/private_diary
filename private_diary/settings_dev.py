744ce217fd7a60b4dc389e14747f2c30b7bbc4fb
Mor8 <fukuya.mori@zeiss.com>
2020-06-22 00:39:39 +0900

    [No. 3013]プロジェクト設定ファイルを本番運用、開発環境、共有したものに分割する

---
+++ private_diary/settings_dev.py
@@ -0,0 +1,47 @@
+from .settings_common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# ロギングの設定
+LOGGING = { 
+    'version': 1, # 1固定
+    'disable_existing_loggers': False, # 既存のロガーの有効無効設定
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        # diaryアプリケーションが利用するロガー
+        'diary': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+
+    # ハンドラーの設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler', # コンソールへ出力するハンドラ
+            'formatter': 'dev'
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
