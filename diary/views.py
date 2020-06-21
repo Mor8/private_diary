388379e4b8979812fd15b2b0155e439a3d178607
Mor8 <fukuya.mori@zeiss.com>
2020-06-21 23:09:59 +0900

    [No. 2799]ビューを追加

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,10 @@
 from django.views import generic
+from .forms import InquiryForm
 
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class InquiryView(generic.FormView):
+    template_name = "inqiry.html"
+    form_class = InquiryForm
